031897261997543dde787858cefe61e98a57aca3
ferkazares <f.cazares.h@edu.utc.mx>
2021-11-22 23:08:00 -0600

    Figuras

---
+++ 06_Figuras/Principal.java
@@ -0,0 +1,16 @@
+/*
+Ejemplo de clase de una figura
+*/
+
+public class Principal{
+
+    public static void main(String[] args){
+
+        //metodo principal
+        //metodo principal
+        //instancia de la clase
+        Figura objfigura = new Figura();
+        objfigura.menu();
+
+    }
+}
